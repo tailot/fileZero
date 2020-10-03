c6d3c49e0d713a2df8889ee6130bc65ae9f79cea
Vincenzo Tilotta <tailot@gmail.com>
2020-10-03 14:05:31 +0200

    Ignored some files from the npm package

--- test/test.sh
+++ test/test.sh
@@ -3,3 +3,7 @@
 ./file2directories test/test.png
 ./directories2file _testtest.png/
 cmp test/test.png new__testtest.png
+
+./file2directories test/path/test.img
+./directories2file _testpathtest.img
+cmp test/path/test.img new__testpathtest.img
