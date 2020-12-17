15a4999223fad46ea308f4af8f4109af70bde99d
Vincenzo Tilotta <tailot@gmail.com>
2020-12-17 15:02:46 +0100

    Added ability to split the token

--- i18n/en_US.js
+++ i18n/en_US.js
@@ -3,5 +3,6 @@ module.exports = {
     WRONGLANGUAGE: 'Your language is not currently supported. I chose English (en_US).',
     LIMITPARAM: 'Wrong parameters!',
     FILENOTFOUND: 'File not found!',
+    SLICERROR: 'Wrong limit set!',
     CALL: () => {return true}
 }
--- i18n/it_IT.js
+++ i18n/it_IT.js
@@ -2,5 +2,6 @@ module.exports = {
     IM: 'it_IT',
     LIMITPARAM: 'Parametri errati!',
     FILENOTFOUND: 'File non trovato.',
+    SLICERROR: 'Limite errato!',
     CALL: () => {return true}
 }
