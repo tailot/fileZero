6fa544fd9f8902c7cc4002be59d1742b4e50f0ad
Vincenzo Tilotta <tailot@gmail.com>
2025-05-28 12:23:37 +0200

    No dependencies

--- i18n/en_US.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    IM: 'en_US',
-    WRONGLANGUAGE: 'Your language is not currently supported. I chose English (en_US).',
-    LIMITPARAM: 'Wrong parameters!',
-    FILENOTFOUND: 'File not found!',
-    SLICERROR: 'Wrong limit set!',
-    CALL: () => {return true}
-}
--- i18n/it_IT.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    IM: 'it_IT',
-    LIMITPARAM: 'Parametri errati!',
-    FILENOTFOUND: 'File non trovato.',
-    SLICERROR: 'Limite errato!',
-    CALL: () => {return true}
-}
